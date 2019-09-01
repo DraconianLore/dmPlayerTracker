2751bd827c17e9a5bc633010181961f26aaa7f1b
Steven Wing <steven.wing82@gmail.com>
2019-08-31 17:11:43 -0700

    login and registration forms complete

--- client/src/modules/LoginRegister.js
+++ client/src/modules/LoginRegister.js
@@ -8,15 +8,15 @@ export default class LoginRegister extends Component {
       auth: ''
     }
   }
-  signup = () => {
+  signup = (event) => {
+    event.preventDefault();
     axios({
       method: 'post',
       url: 'http://localhost:3001/register',
       data: {
-        username: 'Fred',
-        password: '999',
-        email: '1test@test.test'
-
+        username: event.target.Username.value,
+        email: event.target.Email.value,
+        password: event.target.Password.value
       }
     })
       .then((response) => {
@@ -27,13 +27,14 @@ export default class LoginRegister extends Component {
         // });
       })
   }
-  login = () => {
+  login = (event) => {
+    event.preventDefault();
     axios({
       method: 'post',
       url: 'http://localhost:3001/login',
       data: {
-        email: '1test@test.test',
-        password: '999'
+        email: event.target.Email.value,
+        password: event.target.Password.value
 
       }
     })
@@ -45,6 +46,9 @@ export default class LoginRegister extends Component {
         //   message: response.data.message
         // });
       })
+  }
+  loginSignup = () => {
+    this.setState({ new_user: !this.state.new_user })
   }
   test = () => {
     axios({
@@ -66,24 +70,68 @@ export default class LoginRegister extends Component {
 
     return (
       <div>
-        <h1>login/register</h1>
-        <button onClick={this.signup}>Register</button>
-        <button onClick={this.login}>Log in</button>
-        <button onClick={this.test}>Test</button>
-        <div>
-          <h1>Welcome Back</h1>
+        {this.state.new_user || <div>
+        <h1 className="login-signup-title">Welcome Back</h1>
           <div className="login-row">
+            <div className="login-signup">
+              <div className="login-signup-selected-btn">
+                <label className="selected-section">
+                  Log In
+              </label>
+              </div>
+              <div className="login-signup-unselected-btn" onClick={this.loginSignup}>
+                <label className="unselected-section">
+                  Register
+              </label>
+              </div>
+            </div>
             <div className="form-p">
-              <form action="#" method="post">
-                <label className="form-label">Username</label>
-                <input className="login-text" type="text" placeholder="Username" name="Username" required />
+              <form onSubmit={this.login}>
+                <label className="form-label">Email</label>
+                <input className="login-text" type="email" placeholder="Email" autoComplete="email" name="Email" required />
                 <label className="form-label">Password</label>
-                <input className="login-text" type="password" placeholder="Password" name="Password" required />
+                <input className="login-text" type="password" autoComplete="password" placeholder="Password" name="Password" required />
                 <input className="login-submit" type="submit" value="Submit" />
               </form>
             </div>
           </div>
-        </div>
+        </div>}
+        {this.state.new_user && <div>
+        <h1 className="login-signup-title">Welcome to</h1>
+        <h1 className="login-signup-title">dmPlayerTracker</h1>
+          <div className="register-row">
+            <div className="login-signup">
+              <div className="login-signup-unselected-btn" onClick={this.loginSignup}>
+                <label className="unselected-section">
+                  Log In
+              </label>
+              </div>
+              <div className="login-signup-selected-btn">
+                <label className="selected-section">
+                  Register
+              </label>
+              </div>
+            </div>
+            <div className="form-p">
+              <form onSubmit={this.signup}>
+                <input type="hidden" value="disable-autofill"/>
+                <label className="form-label">Username</label>
+                <input className="login-text" type="username" autoComplete="user" placeholder="Username" name="Username" required />
+                <label className="form-label">Email</label>
+                <input className="login-text" type="email" placeholder="Email" autoComplete="email" name="Email" required />
+                <label className="form-label">Password</label>
+                <input className="login-text" type="password" placeholder="Password" autoComplete="new-password" name="Password" required />
+                <input className="login-submit" type="submit" value="Submit" />
+              </form>
+            </div>
+          </div>
+        </div>}
+
+        {/* TESTION SECTION */}
+<h1>login/register test buttons</h1>
+<button onClick={this.signup}>Register</button>
+<button onClick={this.login}>Log in</button>
+<button onClick={this.test}>Test</button>
       </div>
     )
   }
