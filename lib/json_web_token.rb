5c27db57b9929e0b3daa20f4b38ecbd305cd63ad
Steven Wing <steven.wing82@gmail.com>
2019-10-08 19:30:06 -0700

    stupid heroku

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -8,13 +8,13 @@ class JsonWebToken
         payload[:exp] = exp.to_i
         
          # this encodes the user data(payload) with our secret key
-
-        JWT.encode(payload, Rails.application.secrets.SECRET_KEY_BASE)
+         
+        JWT.encode(payload, Rails.application.credentials.secret_key_base)
       end
   
       def decode(token)
         #decodes the token to get user data (payload)
-        body = JWT.decode(token, Rails.application.secrets.SECRET_KEY_BASE)[0]
+        body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
         HashWithIndifferentAccess.new body
         
         # raise custom error to be handled by custom handler
