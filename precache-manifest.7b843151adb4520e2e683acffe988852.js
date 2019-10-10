3d8e1df305ca2c0202eab1803927ff241ae8e70b
Steven Wing <steven.wing82@gmail.com>
2019-10-10 01:28:05 -0700

    Updates

---
+++ precache-manifest.7b843151adb4520e2e683acffe988852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c466e6b785fa04c0caa6",
+    "url": "/dmPlayerTracker/static/js/runtime~main.a169f701.js"
+  },
+  {
+    "revision": "59aec7277508ba0f11c8",
+    "url": "/dmPlayerTracker/static/js/main.297d7a99.chunk.js"
+  },
+  {
+    "revision": "a46d1eae5371c43e4f42",
+    "url": "/dmPlayerTracker/static/js/2.c7e22ce6.chunk.js"
+  },
+  {
+    "revision": "59aec7277508ba0f11c8",
+    "url": "/dmPlayerTracker/static/css/main.cab1e178.chunk.css"
+  },
+  {
+    "revision": "6e0c1a336194f084651d09b170baf962",
+    "url": "/dmPlayerTracker/index.html"
+  }
+];
