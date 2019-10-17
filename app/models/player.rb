4b49cbd34e50e4476fe35eb9bc313e9a43db4355
Steven Wing <steven.wing82@gmail.com>
2019-10-17 00:32:05 -0700

    temporarily disable player-spells

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,7 +6,7 @@ class Player < ApplicationRecord
   has_many :playerSpells
   has_many :playerFeats
   has_many :feats, through: :playerFeats, dependent: :destroy
-  has_many :spells, dependent: :destroy
+  # has_many :spells, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :notes, dependent: :destroy
   validates_presence_of :playerName
