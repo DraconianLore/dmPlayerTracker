7f1b906bf4ebee2e9d1bcfb41365ef6179afdf16
Steven Wing <steven.wing82@gmail.com>
2019-10-23 13:53:49 -0700

    small UI fix

--- client/src/modules/LoginRegister.js
+++ client/src/modules/LoginRegister.js
@@ -92,7 +92,7 @@ export default class LoginRegister extends Component {
               Your screen is too small, please consider running this on a computer as it is unlikely you will be running your campaign from a mobile device.
             </h1>
             <button style={{minWidth: '50px', backgroundColor: 'green', padding: '5px', borderColor: 'darkgreen'}} onClick={this.closeWarning} >
-              <span style={{fontSize: '1.5vw', color: 'antiquewhite'}}>
+              <span style={{fontSize: '1em', color: 'antiquewhite'}}>
                 I understand
                 </span>
             </button>
